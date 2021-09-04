4d593d761c0a5e82b77cca0f70786df20812766b
ItalloGama <Itallo.gama15@gmail.com>
2021-09-04 15:22:05 -0400

    db index clean

--- db/index.js
+++ db/index.js
@@ -24,8 +24,3 @@ const db = mongoose.connection
 
 module.exports = db
 
-// mongodb://127.0.0.1:27017/inspireDataBase
-// mongodb+srv://itallo4:itallo1994@cluster0.
-// .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })gqkt0.mongodb.net/inspireDataBase?retryWrites=true&w=majority
-// let MONGODB_URI = 'mongodb://127.0.0.1:27017/inspireDataBase'
-
